c5c216343094fd410a37b8c20133f5c9fa51b6b0
adamsocrat  <ycs@min2s.com>
2025-12-18 17:51:26 +0100

    refactor: remove unused payment method sync automations
    - Deleted obsolete payment method sync automations from user_automations.xml.
    - Cleaned up imports in __init__.py by removing payment_method_sync.
    - Updated ir.model.access.csv to reflect the removal of payment method access rights.

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,6 @@
 from . import partner_sync
 from . import user_sync
 from . import payment_transaction_sync
-from . import payment_method_sync
 from . import res_users_apikeys
 from . import charging
 from . import charging_session_line
